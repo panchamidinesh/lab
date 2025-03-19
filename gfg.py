f89679f757f364c2750c011a05798b36379e7cbb
bestorbust <panchamid.btech23@rvu.edu.in>
2025-03-19 13:24:01 +0530

    first version of gfg.py

---
+++ gfg.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculating the sum
+sum_result = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum_result)
